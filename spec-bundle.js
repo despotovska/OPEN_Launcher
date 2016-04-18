bd3014a0b3e9e59ca9cf76aa39c50ed0ffccc0ca
Aleksandra Despotovska <adespotovska@endava.net>
2016-04-18 12:00:59 +0200

    remove extensions at spec-bundle

--- spec-bundle.js
+++ spec-bundle.js
@@ -18,8 +18,8 @@ Error.stackTraceLimit = Infinity;
 require('core-js');
 
 require('zone.js/dist/zone');
-require('zone.js/dist/long-stack-trace-zone.js');
-require('zone.js/dist/jasmine-patch.js');
+require('zone.js/dist/long-stack-trace-zone');
+require('zone.js/dist/jasmine-patch');
 
 
 var testing = require('angular2/testing');

b1e9dde394db28837775c35d9ab9f7aa2cdecfa0
Aleksandra Despotovska <adespotovska@endava.net>
2016-06-23 09:55:12 +0200

    modified spec bundle

--- spec-bundle.ts
+++ spec-bundle.ts
@@ -5,7 +5,7 @@
  * karma.conf.js with the karma-webpack plugin. This is the entry
  * file for webpack test. Just like webpack will create a bundle.js
  * file for our client, when we run test, it well compile and bundle them
- * all here! Crazy huh. So we need to do some setup
+ * all here!
 */
 Error.stackTraceLimit = Infinity;
 
@@ -19,12 +19,13 @@ Error.stackTraceLimit = Infinity;
 import 'core-js/es6';
 import 'core-js/es7/reflect';
 import 'reflect-metadata';
-import 'zone.js/dist/zone';
 
 import 'ts-helpers';
 
+import 'zone.js/dist/zone';
 import 'zone.js/dist/sync-test';
 import 'zone.js/dist/jasmine-patch';
+import 'zone.js/dist/long-stack-trace-zone';
 // import 'zone.js/dist/async-test';
 // import 'zone.js/dist/fake-async-test';
 
@@ -43,7 +44,7 @@ Object.assign(global, testing);
   what files we actually want to require or import.
   Below, context will be an function/object with file names as keys.
   using that regex we are saying look in ./src/app and ./test then find
-  any file that ends with spec.js and get its path. By passing in true
+  any file that ends with spec.ts and get its path. By passing in true
   we say do this recursively
 */
 const testContext = require.context('./src', true, /\.spec\.ts/);

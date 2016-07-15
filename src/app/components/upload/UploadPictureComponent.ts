3a0f5afebf40c6845ee8ec206fdf2e3a5181f39d
Aleksandra Despotovska <adespotovska@endava.net>
2016-07-15 14:13:54 +0200

    added validation for uploading invalid format file

--- src/app/components/upload/UploadPictureComponent.ts
+++ src/app/components/upload/UploadPictureComponent.ts
@@ -2,6 +2,7 @@ import {Component, Injector} from 'angular2/core';
 import {Router, CanActivate} from 'angular2/router';
 
 import {AuthService} from '../../shared/services/AuthService';
+import {AlertingService} from '../../shared/services/AlertingService';
 import {UploadPictureService} from './UploadPictureService';
 
 import {TranslatePipe} from 'ng2-translate/ng2-translate';
@@ -30,8 +31,11 @@ export class UploadPictureComponent {
   public selectedFile: File;
   public selectedImage: string;
   public selectedImagePath: string;
+  public filter = /^(image\/jpeg|image\/png|image\/bmp|image\/tiff|image\/gif|image\/jpeg)$/i;
 
-  constructor(private uploadPictureService: UploadPictureService) { }
+  constructor(
+    private uploadPictureService: UploadPictureService,
+    private alertingService: AlertingService) { }
 
   uploadFile(): void {
     this.uploadPictureService.upload(this.selectedFile);
@@ -40,17 +44,19 @@ export class UploadPictureComponent {
 
   onChange(event): void {
     this.selectedFile = event.srcElement.files[0];
-    this.selectedImage = this.selectedFile.name;
+    if (this.filter.test(this.selectedFile.type)) {
+      this.selectedImage = this.selectedFile.name;
 
-    this.selectedImagePath = '';
+      let reader = new FileReader();
+      reader.addEventListener('load', (eventListener) => {
+        this.selectedImagePath = (<IDBOpenDBRequest>eventListener.target).result;
+      }, false);
 
-    let reader = new FileReader();
-
-    reader.addEventListener('load', (eventListener) => {
-      this.selectedImagePath = (<IDBOpenDBRequest>eventListener.target).result;
-    }, false);
-    if (this.selectedFile) {
-      reader.readAsDataURL(this.selectedFile);
+      if (this.selectedFile) {
+        reader.readAsDataURL(this.selectedFile);
+      }
+    } else {
+      this.alertingService.addDanger('UPLOAD_PHOTO_WRONG_FORMAT_MESSAGE');
     }
   }
 

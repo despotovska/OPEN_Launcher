9d3d04c83b8e00ab51d0732bd3eae7469fe0561f
Aleksandra Despotovska <adespotovska@endava.net>
2016-07-25 10:46:47 +0200

    moved games statistics in the db file

--- backend/stats.js
+++
@@ -1,8 +0,0 @@
-var low = require('lowdb');
-var lowdbStorage = require('lowdb/file-async');
-
-var statsPath = require('./paths.js').statsPath;
-
-module.exports = low(statsPath, {
-  storage: lowdbStorage
-});

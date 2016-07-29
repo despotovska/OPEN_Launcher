ebcb64a2e46970706639ff046d3d345c9603ce7e
Daniela Noveska <dnoveska@endava.net>
2016-07-29 12:01:19 +0200

    Adding error message;

--- src/app/components/gameStatistics/GameStatisticComponent.ts
+++ src/app/components/gameStatistics/GameStatisticComponent.ts
@@ -1,5 +1,6 @@
 import {Component} from 'angular2/core';
 import {TranslatePipe} from 'ng2-translate/ng2-translate';
+import {Router, CanActivate} from 'angular2/router';
 
 import {StatisticsService} from '../../shared/services/StatisticsService';
 import {AlertingService} from '../../shared/services/AlertingService';
@@ -7,13 +8,28 @@ import {StatisticViewModel} from '../../shared/models/StatisticViewModel';
 import {Statistic} from '../../shared/models/Statistic';
 import {GameStatisticsModel} from '../../shared/models/GameStatisticsModel';
 import {DeviceType} from '../../shared/enums/UserSettingsEnums';
+import {AuthService} from '../../shared/services/AuthService';
 import {UserSettingsService} from '../../shared/services/UserSettingsService';
+import {appInjector} from '../../../appInjector';
 
 @Component({
   selector: 'statistic',
   templateUrl: `./app/components/gameStatistics/GameStatisticView.html`,
   pipes: [TranslatePipe]
 })
+@CanActivate(
+  (nextInstr: any, currInstr: any) => {
+    let injector: any = appInjector();
+    let authService: AuthService = injector.get(AuthService);
+    let router: Router = injector.get(Router);
+    let isLogged = authService.isLogged();
+
+    if (!isLogged) {
+      router.navigate(['/Login']);
+    }
+    return isLogged;
+  }
+)
 export class GameStatisticComponent {
   public stats: Array<StatisticViewModel> = new Array<StatisticViewModel>();
 
@@ -32,7 +48,7 @@ export class GameStatisticComponent {
         this.stats = data;
         console.log(this.stats.length);
       },
-      err => this.alertingService.addDanger('Грешка при вчитување на статистика.'));
+      err => this.alertingService.addDanger('STATISTIC_ERROR_MESSAGE'));
   }
 
 }

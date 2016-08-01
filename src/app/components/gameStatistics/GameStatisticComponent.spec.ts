82787924ab4fc31f4cd59bcc09738d7f367c9968
Eljesa Zekjiri <ezekjiri@endava.net>
2016-08-01 11:33:16 +0200

    Unit tests with mocks for Game Statistic

--- src/app/components/gameStatistics/GameStatisticComponent.spec.ts
+++ src/app/components/gameStatistics/GameStatisticComponent.spec.ts
@@ -3,21 +3,47 @@ import {
   it,
   inject
 }
- from 'angular2/testing';
+from 'angular2/testing';
 import {provide} from 'angular2/core';
+import {Observable} from 'rxjs/Rx';
 
 import {StatisticsService} from '../../shared/services/StatisticsService';
+import {StatisticsServiceMock} from '../../shared/mocks/StatisticsServiceMock';
 import {AlertingService} from '../../shared/services/AlertingService';
 import {GameStatisticComponent} from './GameStatisticComponent';
-import {UserSettingsService} from '../../shared/services/UserSettingsService';
-import {UserSettingsServiceMock} from '../../shared/mocks/UserSettingsServiceMock';
-import {BaseRequestOptions, Http, Response, ResponseOptions} from 'angular2/http';
 
 describe('GameStatisticComponentTests', () => {
   beforeEachProviders(() => [
     GameStatisticComponent,
     AlertingService,
-    StatisticsService,
-    provide (UserSettingsService, { useClass: UserSettingsServiceMock })
+    provide(StatisticsService, { useClass: StatisticsServiceMock })
   ]);
-});
+
+  it('somename',
+    inject([GameStatisticComponent], (instance) => {
+      // Arrange
+      spyOn(instance.statisticsService, 'getLoggedUserStatisticForGame').and.callThrough();
+      spyOn(instance.alertingService, 'addDanger').and.callFake(() => { });
+
+      // Act
+      // instance.getStatisic('SETS');
+
+      // Assert
+      expect(instance.stats[0].username).toEqual('name');
+      expect(instance.stats.length).toEqual(1);
+    }));
+
+//   it('error',
+//     inject([GameStatisticComponent], (instance) => {
+//       // Arrange
+//       spyOn(instance.alertingService, 'addDanger').and.callFake(() => { });
+//       spyOn(instance.StatisticsService, 'getLoggedUserStatisticForGame').and.callFake(() => { return Observable.throw(new Error()); });
+
+//       // Act
+
+//       // Assert
+//       expect(instance.alertingService.addDanger).toHaveBeenCalledWith('STATISTIC_ERROR_MESSAGE');
+
+
+//     }));
+// });
---
+++ src/app/shared/mocks/StatisticsServiceMock.ts
@@ -0,0 +1,12 @@
+import {Observable} from 'rxjs/Rx';
+import {Duration} from '../models/Duration';
+import {StatisticViewModel} from '../../shared/models/StatisticViewModel';
+
+export class StatisticsServiceMock {
+
+  getLoggedUserStatisticForGame(game: string) {
+    let data: Array<StatisticViewModel> = new Array<StatisticViewModel>();
+    data[0] = new StatisticViewModel('name', 'joystick', new Duration(0, 10, 15), 5, 17);
+    return Observable.of(data);
+  }
+}
